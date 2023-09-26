9a58fb607d0e85fc8bbfdada919b586875d62e91
Duncan MacLennan <dcupmusic@gmail.com>
2023-09-26 14:13:00 +1000

    fix

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-# Initialize an empty string to store the alphabet
-alphabet_str = ""
-
 # Loop through the ASCII values of the lowercase alphabet (97-122)
 for i in range(97, 123):
-    # Append the character corresponding to the ASCII value to alphabet_str
-    alphabet_str += chr(i)
-
-# Print the complete alphabet string without a new line
-print(alphabet_str, end="")
+    # Print the character corresponding to the ASCII value without a new line
+    print(chr(i), end="")
