1516390589f3aa24303784afc44d3f362a22b778
Duncan MacLennan <dcupmusic@gmail.com>
2023-09-27 08:55:29 +1000

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-# Loop through the ASCII values of the lowercase alphabet (97-122)
-for i in range(97, 123):
-    # Skip the ASCII value for 'q' (113) and 'e' (101)
-    if i != 113 and i != 101:
-        print("{:s}".format(chr(i)), end="")
+for value in range(97, 123):
+    if value != 101 and value != 113:
+        print("{:s}".format(chr(value)), end="")
