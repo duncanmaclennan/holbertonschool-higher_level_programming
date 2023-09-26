4fa43c169ad53a86cd3917c3d5708ee8b809c8c6
Duncan MacLennan <dcupmusic@gmail.com>
2023-09-26 14:26:58 +1000

    fix

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 # Loop through the ASCII values of the lowercase alphabet (97-122)
 for i in range(97, 123):
     # Print the character corresponding to the ASCII value without a new line
-    print(chr(i), end="")
+    print("{:s}".format(chr(i)), end="")
