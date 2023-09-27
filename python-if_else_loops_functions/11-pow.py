118fbd38092afe2c94e891a764faadc8b1f3bdbb
Duncan MacLennan <dcupmusic@gmail.com>
2023-09-27 10:10:22 +1000

    task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    # Special case: b is zero
-    if b == 0:
-        return 1
 
-    # Special case: b is negative
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    result = 1
-    for _ in range(b):
-        result *= a
-
-    return result
+    return a ** b
