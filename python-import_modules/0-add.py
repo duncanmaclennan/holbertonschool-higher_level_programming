f465a92fbbe5f2d78ef8d08b5fbd26101c4c6e6f
Duncan MacLennan <dcupmusic@gmail.com>
2023-09-28 09:03:55 +1000

    task 0 and readme

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from add_0 import add
+
+
+def main():
+
+    a = 1
+
+    b = 2
+
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    main()
