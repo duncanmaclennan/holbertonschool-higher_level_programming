804db67326121e8f51c03b693a4e70a1110db296
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-03 14:22:36 +1100

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    # Ensure that tuple_a and tuple_b have at least two elements by filling with zeros if necessary
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-
-    # Sum the first two elements of each tuple and return as a new tuple
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    zero_tuple = (0, 0)
+    tuple_a = tuple_a + zero_tuple
+    tuple_b = tuple_b + zero_tuple
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
