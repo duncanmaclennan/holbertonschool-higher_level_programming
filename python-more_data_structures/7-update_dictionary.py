ef7640c021d14029e269ac659f968c6aac7fcae9
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-03 15:49:31 +1100

    task 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Update the value for the given key or add a new key-value pair
+    a_dictionary[key] = value
+
+    # Return the modified dictionary
+    return a_dictionary
