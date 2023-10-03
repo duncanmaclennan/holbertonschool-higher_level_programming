99d67ec1874ba7e26363292ced2cf90ebaffebbb
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-03 14:19:52 +1100

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-
-    # Iterate through each row in the matrix
-    for row in matrix:
-        # Initialize a string to store the row's elements
-        row_string = ''
-
-        # Iterate through each element in the row
-        for i, element in enumerate(row):
-            # Add the element to row_string, followed by a space if not the last element
-            row_string += "{:d}".format(element)
-            if i != len(row) - 1:
-                row_string += " "
-
-        # Print the row
-        print(row_string)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if (j != 0):
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+        print("")
