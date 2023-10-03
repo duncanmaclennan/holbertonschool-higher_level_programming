ee0ea0e833717608af4d5acac9f172bc30e2977e
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-03 14:32:30 +1100

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 def delete_at(my_list=[], idx=0):
-
-    # Validate that the index is not negative and within range
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > len(my_list):
         return my_list
 
-    # Create a new list without the element at index idx
-    my_list = my_list[:idx] + my_list[idx + 1:]
-
+    for i in range(len(my_list)):
+        if i == idx:
+            my_list.remove(my_list[i])
     return my_list
