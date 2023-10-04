14d994075f1a700dcce66405276d1add33c03fe1
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-05 10:56:37 +1100

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             printed_count += 1
-        except IndexError:  # This will catch attempts to access out-of-range indices
+        except:  # This will catch attempts to access out-of-range indices
             break
     print()  # This ensures we print a newline after all elements
     return printed_count
