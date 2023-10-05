c5f8d5d4e5c82d48474b3f85eb7f2564272a08c3
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-05 11:35:27 +1100

    fix

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if isinstance(value, int):
-        try:
-            print("{:d}".format(value))
-            return True
-        except:
-            return False
-    else:
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
         return False
