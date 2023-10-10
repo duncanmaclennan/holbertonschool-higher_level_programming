296e9b960cd07cb605d1333b530d1e6097f602de
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-10 11:24:14 +1100

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 class Square:
-    """
-    An empty class
+    """The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        none
     """
