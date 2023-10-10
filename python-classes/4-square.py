2a61166789e894b281429872cb1c8796d3019d11
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-10 13:12:33 +1100

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module for defining a Square with conditions, area calculation, and property setters/getters.
+"""
+
+
+class Square:
+    """
+    Class that defines a square with a private instance attribute size.
+    Includes input validation, a method to compute the area, and property setters/getters.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization method with an optional size argument.
+        """
+        self.size = size  # This will use the setter method
+
+    @property
+    def size(self):
+        """
+        Getter for size attribute.
+        Returns the size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for size attribute with type and value validation.
+        """
+        # Type verification
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        # Value verification
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        # Assigning the value
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the current square area.
+        """
+        return self.__size ** 2
