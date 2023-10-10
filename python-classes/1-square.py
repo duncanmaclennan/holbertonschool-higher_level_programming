340db1ed6800c55599f3ee2731dcf0bed99d37d6
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-10 13:05:40 +1100

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for defining a Square.
+"""
+
+
+class Square:
+    """
+    Class that defines a square with a private instance attribute size.
+    """
+
+    def __init__(self, size):
+        """
+        Initialization method with a size argument.
+        """
+        self.__size = size
