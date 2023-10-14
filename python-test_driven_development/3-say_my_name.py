a2b367a88790378652ef5f771e0bda068276cde8
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-14 13:37:47 +1100

    task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""A module that print name"""
+"""
+Function prints "My name is <first name> <last name>"
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """A function that print name"""
+    """
+    Args:
+        first_name: str
+        last_name: str
 
-    if not isinstance(first_name, str):
+    Return:
+        None
+
+    Raises:
+        TypeError: first/last name must be a string
+    """
+    if not isinstance(first_name, (str)):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if not isinstance(last_name, (str)):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {0} {1}".format(first_name, last_name))
