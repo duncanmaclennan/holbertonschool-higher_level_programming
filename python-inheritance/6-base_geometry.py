9e28347f8b2e84ae0d747edabbacef27205e0e1c
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-24 14:13:16 +1100

    t6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a module"""
+
+
+class BaseGeometry:
+    """Define a function to raise exception"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
