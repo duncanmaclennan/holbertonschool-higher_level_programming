2e918e192a9f08f4c7641b63b815710ffe569d54
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-24 15:38:00 +1100

    t3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a module"""
+import json
+
+
+def to_json_string(my_obj):
+    """Define a functoin that returns JSON representation of object"""
+
+    return json.dumps(my_obj)
