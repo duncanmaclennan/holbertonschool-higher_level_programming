3d6ed357d292703188e787824ca746ee9dd7241d
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-24 14:34:54 +1100

    t0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Function reads text file and prints to stdout
-"""
+"""Define a module"""
 
 
 def read_file(filename=""):
-    """
-    reads input file and prints to stdout
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Define function that read file"""
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        contents = file.read()
+        print(contents, end="")
