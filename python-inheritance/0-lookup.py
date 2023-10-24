e09e7c7ce253795407f33b8b59841a456e323ef3
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-24 13:55:09 +1100

    t0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Return list available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Return list available attributes and methods of an object"""
+
+    return dir(obj)
