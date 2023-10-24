93f96eb14d2d33744fa6349d59963e20f884ba6a
Duncan MacLennan <dcupmusic@gmail.com>
2023-10-24 14:22:17 +1100

    t8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a module"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Inherite from a class"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
