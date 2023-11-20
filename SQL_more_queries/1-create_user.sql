3b911b7c19464e2d8f6cd7aa0a7fddcbb6399288
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-21 10:29:41 +1100

    t1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,5 @@
 -- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
