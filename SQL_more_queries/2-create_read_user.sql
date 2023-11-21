747201aa5c06c982d89ed4fe2558d5dceaca8c70
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-21 11:48:01 +1100

    t2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- create read user
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
