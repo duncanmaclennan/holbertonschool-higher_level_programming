f0da75a55fdd163ace0ede0cb659456a87fad9e5
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-23 12:41:50 +1100

    t14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- my genres
-SELECT genres.name 
-FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-JOIN genres ON tv_show_genres.genre_id = genres.id 
-WHERE tv_shows.title = 'Dexter' 
-ORDER BY genres.name ASC;
+SELECT tv_genres.name AS name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
