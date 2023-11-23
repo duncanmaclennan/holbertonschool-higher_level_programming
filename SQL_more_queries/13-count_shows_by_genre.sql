fa915a57f826ed594f387651ef8991178637f9ca
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-23 11:29:41 +1100

    t13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- count shows by genre
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows 
-FROM genres 
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id 
-GROUP BY genres.name 
-HAVING COUNT(tv_show_genres.show_id) > 0 
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
