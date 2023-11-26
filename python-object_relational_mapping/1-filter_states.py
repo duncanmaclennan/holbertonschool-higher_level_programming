fac5681f3c06788796564eb541528091ea09ad81
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-27 10:04:43 +1100

    all ts

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N) from hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # mydb is connection object
+    mydb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3])
+
+    # cursorObj is object to execute queries
+    cursorObj = mydb.cursor()
+
+    # cursor object execute SQL query but not return value
+    cursorObj.execute("SELECT * FROM states\
+                      WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows and display them
+    myresult = cursorObj.fetchall()
+
+    for row in myresult:
+        print(row)
+
+    # shut cursor and database connection
+    cursorObj.close()
+    mydb.close()
