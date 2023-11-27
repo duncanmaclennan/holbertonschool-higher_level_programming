68babb3e8585e1b229830b53258c73713a9a615c
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-28 09:09:21 +1100

    t14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
 """
-Prints all City objects from the database hbtn_0e_14_usa
+Script that prints all City objects from the database hbtn_0e_14_usa
 """
-
+import MySQLdb
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
-from model_state import Base, State
-from model_city import City
+from model_city import Base, City
+from model_state import State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Create engine
-    engine = create_engine(
-        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
-
-    # Create a configured "Session" class
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
-
-    # Query all cities joined with states, ordered by city id
-    cities = session.query(City).options(
-        joinedload(City.state)).order_by(City.id).all()
-
-    # Print each city
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
-
-    # Close the session
+    city_print = session.query(City, State).join(State)
+    for city, state in city_print:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""
+Class definition City
+"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
 
 class City(Base):
-    """Represents a city for a MySQL database."""
-
+    """
+    Class City that inherits from Base
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
