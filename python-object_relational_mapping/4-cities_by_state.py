b3857ad75d5d0baaf442d489cf7210e8a80db174
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-27 15:47:03 +1100

    t4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
 
     mydb = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         database=sys.argv[3])
