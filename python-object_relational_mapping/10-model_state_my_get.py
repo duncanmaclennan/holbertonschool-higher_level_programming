a05460c412ff0a49b902f74ae811377ac877a15c
Duncan MacLennan <dcupmusic@gmail.com>
2023-11-28 08:47:26 +1100

    t10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """Prints the State object with the name passed as argument from hbtn_0e_6_usa database."""
 
+import MySQLdb
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-
-    engine = create_engine(
-        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
-
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    if state:
-        print(state.id)
-    else:
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+    cursor = database.cursor()
+    search_term = sys.argv[4]
+    cursor.execute("SELECT id FROM states WHERE name LIKE %s ORDER BY id;",
+                   (search_term,))
+    rows = cursor.fetchall()
+    if not rows:
         print("Not found")
-
-    session.close()
+    else:
+        for row in rows:
+            print("{}".format(row[0]))
+    cursor.close()
+    database.close()
